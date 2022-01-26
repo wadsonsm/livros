fa6298d8ed6bf6c094cd7bc1cd863284282e6c6e
wadson <wadson.matos@lockton.com>
2022-01-26 18:07:29 -0300

    incompleto com o vue

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
 
-$(document).ready(function () {
+//$(document).ready(function () {
     // function onAdd() {
     //     var $ul, li, $li, $label, $div, livro;
     //     livro = $('.js-novo-livro').val();
@@ -39,52 +39,52 @@ $(document).ready(function () {
     //     $el.closest('li').toggleClass('removido');
     // }
 
-    function onAdd(){
-    var $ul, li, $li, $label, $div, livro, autor;
-    livro = $('.js-novo-livro').val();
-    autor = $('.js-novo-autor').val();
+//     function onAdd(){
+//     var $ul, li, $li, $label, $div, livro, autor;
+//     livro = $('.js-novo-livro').val();
+//     autor = $('.js-novo-autor').val();
 
-    console.log(livro + ' -- ' + autor);
-    //valida se livro está vazio
-    if(livro ===''){
-        return;
-    }
+//     console.log(livro + ' -- ' + autor);
+//     //valida se livro está vazio
+//     if(livro ===''){
+//         return;
+//     }
 
-    $ul = $('ul');
-    $li = $('<li>').appendTo($ul);
-    $div = $('<div>')
-            .addClass('checkbox')
-            .appendTo($li);
+//     $ul = $('ul');
+//     $li = $('<li>').appendTo($ul);
+//     $div = $('<div>')
+//             .addClass('checkbox')
+//             .appendTo($li);
 
-    $label = $('<label>').appendTo($div);
-    $('<input>')
-        .attr('type', 'checkbox')
-        .addClass('js-livro')
-        .attr('name','list')
-        .click(toggleRemovido)
-        .appendTo($label);
+//     $label = $('<label>').appendTo($div);
+//     $('<input>')
+//         .attr('type', 'checkbox')
+//         .addClass('js-livro')
+//         .attr('name','list')
+//         .click(toggleRemovido)
+//         .appendTo($label);
 
-    $('<big>')
-            .appendTo($label)
-            .append(livro);
+//     $('<big>')
+//             .appendTo($label)
+//             .append(livro);
 
-    $label.append(" - ");
+//     $label.append(" - ");
 
-    $('<small>')
-            .appendTo($label)
-            .append(autor);
-    $('.js-novo-livro, .js-novo-autor').val('');
+//     $('<small>')
+//             .appendTo($label)
+//             .append(autor);
+//     $('.js-novo-livro, .js-novo-autor').val('');
 
-}
+// }
 
-    function toggleRemovido(ev)
-    {
-        var $el;
-        $el = $(ev.currentTarget);
-        $el.closest('li').toggleClass('removido');
-    }
+//     function toggleRemovido(ev)
+//     {
+//         var $el;
+//         $el = $(ev.currentTarget);
+//         $el.closest('li').toggleClass('removido');
+//     }
 
-    $('.js-add').click(onAdd);
-    $('.js-livro').click(toggleRemovido);
+//     $('.js-add').click(onAdd);
+//     $('.js-livro').click(toggleRemovido);
 
-})
+// })
